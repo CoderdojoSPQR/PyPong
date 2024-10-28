a86ee3834a585713f9acdd1b1a7d1a39f1cf791c
Dag7 <dag7@protonmail.com>
2024-10-28 14:58:34 +0100

    l_screen and a_screen are now capitalized

--- code/pong_3_completa_con_punteggio.py
+++ code/pong_3_completa_con_punteggio.py
@@ -4,18 +4,18 @@ import sys
 pygame.init()
 
 # Dimensioni della finestra e colori
-L_SCHERMO, A_SCHERMO = 800, 600
-BIANCO = (255, 255, 255)
+L_schermo, A_schermo = 800, 600
+bianco = (255, 255, 255)
 
 # Inizializzazione della finestra
-schermo = pygame.display.set_mode((L_SCHERMO, A_SCHERMO))
+schermo = pygame.display.set_mode((L_schermo, A_schermo))
 
 # Posizione e velocità della pallina
-x_pallina, y_pallina = L_SCHERMO // 2, A_SCHERMO // 2
+x_pallina, y_pallina = L_schermo // 2, A_schermo // 2
 dx_pallina, dy_pallina = 5, 5
 grandezza_pallina = 10
 
-paddle1_y, paddle2_y = A_SCHERMO // 2, A_SCHERMO // 2
+paddle1_y, paddle2_y = A_schermo // 2, A_schermo // 2
 paddle_larghezza, paddle_altezza = 10, 100
 velocita_paddle = 7
 
@@ -33,11 +33,11 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and paddle1_y > 0:
         paddle1_y = paddle1_y - velocita_paddle
-    if keys[pygame.K_s] and paddle1_y < A_SCHERMO - paddle_altezza:
+    if keys[pygame.K_s] and paddle1_y < A_schermo - paddle_altezza:
         paddle1_y = paddle1_y + velocita_paddle
     if keys[pygame.K_UP] and paddle2_y > 0:
         paddle2_y = paddle2_y - velocita_paddle
-    if keys[pygame.K_DOWN] and paddle2_y < A_SCHERMO - paddle_altezza:
+    if keys[pygame.K_DOWN] and paddle2_y < A_schermo - paddle_altezza:
         paddle2_y = paddle2_y + velocita_paddle
 
     # Movimento della pallina
@@ -47,7 +47,7 @@ while True:
     # COLLISIONI
     # =======
     # Collisione con il bordo superiore e inferiore
-    if y_pallina <= grandezza_pallina or y_pallina >= A_SCHERMO - grandezza_pallina:
+    if y_pallina <= grandezza_pallina or y_pallina >= A_schermo - grandezza_pallina:
         dy_pallina = dy_pallina * -1
 
     # Collisione con il paddle sinistro
@@ -57,30 +57,30 @@ while True:
         x_pallina = paddle_larghezza + grandezza_pallina 
 
     # Collisione con il paddle destro
-    if (x_pallina + grandezza_pallina >= L_SCHERMO - paddle_larghezza and
+    if (x_pallina + grandezza_pallina >= L_schermo - paddle_larghezza and
         paddle2_y < y_pallina < paddle2_y + paddle_altezza):
         dx_pallina = dx_pallina * -1
-        x_pallina = L_SCHERMO - paddle_larghezza - grandezza_pallina 
+        x_pallina = L_schermo - paddle_larghezza - grandezza_pallina 
 
     # Reset della pallina se va oltre i bordi
-    if x_pallina < 0 or x_pallina > L_SCHERMO:
-        x_pallina, y_pallina = L_SCHERMO // 2, A_SCHERMO // 2
+    if x_pallina < 0 or x_pallina > L_schermo:
+        x_pallina, y_pallina = L_schermo // 2, A_schermo // 2
         dx_pallina = -dx_pallina
 
         if x_pallina < 0:
             punti_giocatore_2 = punti_giocatore_2 + 1
 
-        if x_pallina > L_SCHERMO: 
+        if x_pallina > L_schermo: 
             punti_giocatore_1 = punti_giocatore_1 + 1
 
 
     # Disegno di tutto 
     schermo.fill((0, 0, 0))
-    pygame.draw.circle(schermo, BIANCO, (x_pallina, y_pallina), grandezza_pallina)
-    pygame.draw.rect(schermo, BIANCO, (0, paddle1_y, paddle_larghezza, paddle_altezza))
-    pygame.draw.rect(schermo, BIANCO, (L_SCHERMO - paddle_larghezza, paddle2_y, paddle_larghezza, paddle_altezza))
+    pygame.draw.circle(schermo, bianco, (x_pallina, y_pallina), grandezza_pallina)
+    pygame.draw.rect(schermo, bianco, (0, paddle1_y, paddle_larghezza, paddle_altezza))
+    pygame.draw.rect(schermo, bianco, (L_schermo - paddle_larghezza, paddle2_y, paddle_larghezza, paddle_altezza))
     font = pygame.font.Font(None, 36)
-    testo_punti = font.render(f"{punti_giocatore_1} - {punti_giocatore_2}", True, BIANCO)
-    schermo.blit(testo_punti, (L_SCHERMO // 2 - 20, 20))
+    testo_punti = font.render(f"{punti_giocatore_1} - {punti_giocatore_2}", True, bianco)
+    schermo.blit(testo_punti, (L_schermo // 2 - 20, 20))
     pygame.display.update()
     pygame.time.delay(30)
